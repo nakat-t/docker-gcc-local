f467a7d1aacf72c23948b7d9791af54214096808
nakat-t <armaiti.wizard@gmail.com>
2020-06-24 16:36:18 +0900

    Add autotest
    Add the docker hub autotest file.
    
    The test is simple: make sure that the added commands are executable and the version is correct.

---
+++ gcc/8/run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eu
+
+version=8
+
+for cmd in gcc gcc-${version} g++ g++-${version}
+do
+  result=0
+  output1=$(${cmd} -dumpversion 2>&1) || result=$?
+  if [ ! "${result}" = "0" ]; then
+    echo >&2 "ERROR: command '${cmd}' not found."
+    exit ${result}
+  fi
+  output2=$(echo ${output1} | grep "${version}") || result=$?
+  if [ ! "${result}" = "0" ]; then
+    echo >&2 "ERROR: wrong version of command '${cmd}'."
+    exit ${result}
+  fi
+done
+
+for cmd in gcc-ar gcc-ar-${version}
+do
+  result=0
+  output1=$(${cmd} --version 2>&1) || result=$?
+  if [ ! "${result}" = "0" ]; then
+    echo >&2 "ERROR: command '${cmd}' not found."
+    exit ${result}
+  fi
+done
